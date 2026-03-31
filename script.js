59e926b6b592daf211e16166cd8f2e365801ff19
Gustavo <dev.gustavo97@gmail.com>
2026-03-31 15:52:05 -0300

    feat: map DOM elements and add click event listener

---
+++ script.js
@@ -0,0 +1,10 @@
+const amount = document.getElementById("amount");
+const fromCurrency = document.getElementById("fromCurrency");
+const toCurrency = document.getElementById("toCurrency");
+const convertButton = document.getElementById("convertBtn");
+const result = document.getElementById("result");
+
+    
+convertButton.addEventListener("click", () => {
+    console.log('botao clicado');
+});
